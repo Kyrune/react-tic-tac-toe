3db4d729840412ac9ba415ba71a2e498bc1fed98
Kyrune <jn-01@centurytel.net>
2020-06-22 10:58:17 -0500

    Create function shells/import Board component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Board from './Board';
 
 const Game = () => {
+
+    const handleClick = () => {
+
+    };
+
+
+    const jumpTo = () => {
+
+    };
+
+    const renderMoves = () => {
+        
+    };
+    
     return (
-        <div>
-            Tic Tac Toe Game
-        </div>
+        <Board onClick={handleClick} />
     );
 };
 
