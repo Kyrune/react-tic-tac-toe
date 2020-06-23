8ad936c3f022d674a7fee187dd78ab4341f49869
Kyrune <jn-01@centurytel.net>
2020-06-23 09:51:40 -0500

    Add styling to Square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+const style = {
+    background: '#464545',
+    border: '2px solid black',
+    fontSize: '30px',
+    color: 'oldlace',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+};
+
 const Square = ({ value, onClick}) => 
     (
-        <button onClick={onClick}>
+        <button style={style} onClick={onClick}>
             {value}
         </button>
     );
