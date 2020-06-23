14665274f27b24c4ab7ceddabd2e52e55465efda
Kyrune <jn-01@centurytel.net>
2020-06-23 09:51:51 -0500

    Add styling to Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Square from './Square';
 
+const style = {
+    border: '4px solid black',
+    borderRadius: '10px',
+    width: '250px',
+    height: '250px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+};
+
 const Board = ({ squares, onClick }) => 
     (
         // Change to loop
-        <div>
+        <div style={style}>
             <Square value="1" onClick={() => onClick("dummy value")} />
             <Square value="2" onClick={() => onClick("dummy value")} />
             <Square value="3" onClick={() => onClick("dummy value")} />
