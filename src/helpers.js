d752156571c744e8a3347cd42ac5f6dada0eee8b
Kyrune <jn-01@centurytel.net>
2020-06-24 10:12:46 -0500

    Add if statement to check squares

--- src/helpers.js
+++ src/helpers.js
@@ -9,4 +9,20 @@ export function calculateWinner(squares) {
         [0, 4, 8],
         [2, 4, 6]
     ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 };
+
+// const squares = [
+//     null, null, null,
+//     'X', 'X', 'X',
+//     null, null, null
+// ];
+  
+// console.log(calculateWinner(squares));
