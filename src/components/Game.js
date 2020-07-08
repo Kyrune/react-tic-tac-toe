63644254c111a4835d32ecc6a8f960d0ac3d961f
Kyrune <jn-01@centurytel.net>
2020-07-08 10:18:31 -0500

    Map thru history and list buttons

--- src/components/Game.js
+++ src/components/Game.js
@@ -33,9 +33,16 @@ const Game = () => {
     };
 
     const renderMoves = () => (
-        <button onClick={() => setHistory(Array(9).fill(null))}>
-            Start Game
-        </button>
+        history.map((_step, move) => {
+            const destination = move ? `Go to move#${move}` : 'Go to start';
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>
+                        {destination}
+                    </button>
+                </li>
+            )
+        })
     );
     
     return (
